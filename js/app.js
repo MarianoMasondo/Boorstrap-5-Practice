28aab030e1e1afd1c4d8ce8142290191d2bda692
Mariano Masondo <nanomason@gmail.com>
2024-01-07 16:31:46 -0300

    Galería Page

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const modalImagen = document.querySelector("#modal-imagen");
+
+modalImagen.addEventListener("show.bs.modal", function (event) {
+  const enlace = event.relatedTarget;
+  const rutaImagen = enlace.getAttribute("data-bs-imagen")
+
+  //consturir imagen
+  const imagen = document.createElement("IMG");
+  imagen.src = `img/${rutaImagen}.jpg`;
+  imagen.classList.add("img-fluid");
+  imagen.alt = "Imagen galeria";
+
+  const contenidoModal = document.querySelector(".modal-body");
+  contenidoModal.appendChild(imagen);
+});
+modalImagen.addEventListener("hidden.bs.modal", function(){
+  const contenidoModal = document.querySelector(".modal-body");
+  contenidoModal.textContent = "";
+})
